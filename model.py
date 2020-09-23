10c9c01e794fbc292e2349c1c49b03a8d65523ee
NOOR <ehsanmirhashemi41@gmail.com>
2020-09-23 11:37:58 +0330

    find the best model

--- model.py
+++ model.py
@@ -6,3 +6,7 @@ import pandas as pd
 train_data = pd.read_csv('dataset/train.csv')
 test_data = pd.read_csv('dataset/test.csv')
 
+# The preprocessor function will be implement here...
+from preprocessing_data import processor_data
+
+clean_data = processor_data(train_data, test_data)
--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,25 +1,27 @@
 # Import libraries
-from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
-def preprocessing(train, test):
+
+def processor_data(train, test):
+    
     train_columns = ['Gender',
-                 'Age',
-                 'Driving_License',
-                 'Region_Code',
-                 'Previously_Insured',
-                 'Vehicle_Age',
-                 'Vehicle_Damage',
-                 'Annual_Premium',
-                 'Policy_Sales_Channel',
-                 'Vintage'] 
-
+                     'Age',
+                     'Driving_License',
+                     'Region_Code',
+                     'Previously_Insured',
+                     'Vehicle_Age',
+                     'Vehicle_Damage',
+                     'Annual_Premium',
+                     'Policy_Sales_Channel',
+                     'Vintage'] 
+    
     x_train = train[train_columns]
     # Or
     # x_train = train.iloc[:, :11]
-    
+        
     y_train = train['Response']
-    
+        
     # Preprocessing
     columns_need_dummy = ['Gender',
                           'Driving_License',
@@ -28,14 +30,22 @@ def preprocessing(train, test):
                           'Vehicle_Damage',
                           'Vehicle_Age'
                           ]
-        
+       
+    # Dummmy values     
     x_train = pd.get_dummies(
         x_train,
         columns = columns_need_dummy)
-    
+        
     x_test = pd.get_dummies(
         test,
         columns = columns_need_dummy)
+        
+        
+        
+    # Time to scaling... 
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.fit_transform(x_test)
     
     return {'x_train': x_train,
             'x_test': x_test,
